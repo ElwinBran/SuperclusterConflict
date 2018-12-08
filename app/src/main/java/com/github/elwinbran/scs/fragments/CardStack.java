6671537ae55873f58c93c69b6b8e37fc0cd6e7c2
ElwinBran <elwinbran@gmail.com>
2018-12-08 21:09:56 +0100

    Added some fragments that will be implemented fully later. These should be all fragments that the game requires.

---
+++ app/src/main/java/com/github/elwinbran/scs/fragments/CardStack.java
@@ -0,0 +1,8 @@
+package com.github.elwinbran.scs.fragments;
+
+import android.support.v4.app.Fragment;
+
+public class CardStack extends Fragment
+{
+    
+}
---
+++ app/src/main/java/com/github/elwinbran/scs/fragments/SingleCard.java
@@ -0,0 +1,8 @@
+package com.github.elwinbran.scs.fragments;
+
+import android.support.v4.app.Fragment;
+
+public class SingleCard extends Fragment
+{
+
+}
