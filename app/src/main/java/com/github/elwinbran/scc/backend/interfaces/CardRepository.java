7f5942d366f6f7a4a19da4c36a7736e80af82ce1
ElwinBran <elwinbran@gmail.com>
2019-01-09 15:19:49 +0100

    Made required interface to wrap the ugly retrofit

--- app/src/main/java/com/github/elwinbran/scc/backend/interfaces/CardRepository.java
+++ app/src/main/java/com/github/elwinbran/scc/backend/interfaces/CardRepository.java
@@ -1,9 +1,13 @@
 package com.github.elwinbran.scc.backend.interfaces;
 
+import com.github.elwinbran.android.scc.CardGroups;
+
 /**
+ * An interface for retrieving the groups of cards.
  *
+ * @author Elwin Slokker
  */
 public interface CardRepository
 {
-
+    public CardGroups cardGroups();
 }
