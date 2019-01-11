94e2ea786428644075a2515fb96c77eaf051c145
ElwinBran <elwinbran@gmail.com>
2019-01-11 18:51:56 +0100

    Renamed and refactored, because it is a ViewModel

--- app/src/main/java/com/github/elwinbran/scc/service/GameStatisticsViewModel.java
+++ app/src/main/java/com/github/elwinbran/scc/service/GameStatisticsViewModel.java
@@ -10,15 +10,28 @@ import com.github.elwinbran.scc.backend.interfaces.Statistic;
  *
  * @author Elwin Slokker
  */
-public abstract class GameStatistics extends ViewModel
+public abstract class GameStatisticsViewModel extends ViewModel
 {
 
+    /**
+     *
+     * @return A special model that updates automatically.
+     */
     public abstract LiveData<Statistic> retrieve();
 
+    /**
+     * Adds another 'win' to the count.
+     */
     public abstract void incrementWins();
 
+    /**
+     * Adds another 'loss' to the count.
+     */
     public abstract void incrementLosses();
 
+    /**
+     * Puts both the 'wins' and 'losses' to zero, effectively resetting.
+     */
     public abstract void reset();
 
 }
