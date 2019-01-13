27da06c1de7065ac311b050d738ba224297be5b6
ElwinBran <elwinbran@gmail.com>
2019-01-13 13:22:33 +0100

    Added the stub for fragment

---
+++ app/src/main/java/com/github/elwinbran/android/scc/fragments/DemoCard.java
@@ -0,0 +1,13 @@
+package com.github.elwinbran.android.scc.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * The UI representation of a card in the demo.
+ *
+ * @author Elwin Slokker
+ */
+public class DemoCard extends Fragment
+{
+    
+}
