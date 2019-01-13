0d5491d54f2fa6a0976238cb2b1c76516ea6e1ca
ElwinBran <elwinbran@gmail.com>
2019-01-13 14:00:31 +0100

    stuff

--- app/src/main/java/com/github/elwinbran/android/scc/app/NormalGame.java
+++ app/src/main/java/com/github/elwinbran/android/scc/app/NormalGame.java
@@ -1,17 +1,34 @@
 package com.github.elwinbran.android.scc.app;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
 
+import com.github.elwinbran.android.scc.api.AppDatabase;
 import com.github.elwinbran.android.scc.app.R;
 
+/**
+ * The normal game activity of Supercluster Conflict.
+ * TODO: Most of this class' code is WIP and meant for the demo only!
+ *
+ * @author Elwin Slokker
+ */
 public class NormalGame extends FullscreenCompatActivity
 {
+    private AppDatabase gameStateDB;
+
+    private final GameStateProperty state = new GameStateProperty(null);
+
     @Override
     public void onCreate(Bundle savedInstance)
     {
         setContentView(R.layout.activity_normal_game);
         super.assign(findViewById(R.id.root_constraint_layout));
         super.onCreate(savedInstance);
+        gameStateDB = AppDatabase.getInstance(this);
+        final RecyclerView playerCardDisplay = findViewById(R.id.demo_card_holder_player);
+        final RecyclerView oppponentCardDisplay = findViewById(R.id.demo_card_holder_opponent);
+        final Button addCardButton = findViewById(R.id.add_button);
         /*
         So basically some extra thread is required to host the gameloop.
         This is a two way system...
@@ -20,5 +37,15 @@ public class NormalGame extends FullscreenCompatActivity
         */
     }
 
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+    }
 
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+    }
 }
