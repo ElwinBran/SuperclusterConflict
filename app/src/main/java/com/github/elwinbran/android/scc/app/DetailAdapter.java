0957551ede5fe7a45cd6f0355da829dbac860ca6
ElwinBran <elwinbran@gmail.com>
2019-01-15 10:13:44 +0100

    Started working on the recyclerview for detail items

---
+++ app/src/main/java/com/github/elwinbran/android/scc/app/DetailAdapter.java
@@ -0,0 +1,33 @@
+package com.github.elwinbran.android.scc.app;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * The manager for the card detail list.
+ *
+ * @author Elwin Slokker
+ */
+public class DetailAdapter extends RecyclerView.Adapter<DetailViewHolder>
+{
+
+    @NonNull
+    @Override
+    public DetailViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+    {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DetailViewHolder detailViewHolder, int i)
+    {
+
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/github/elwinbran/android/scc/app/DetailViewHolder.java
@@ -0,0 +1,18 @@
+package com.github.elwinbran.android.scc.app;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * The view holder for the detail overview list of cards in the demo.
+ *
+ * @author Elwin Slokker
+ */
+public class DetailViewHolder extends RecyclerView.ViewHolder
+{
+    public DetailViewHolder(@NonNull View itemView)
+    {
+        super(itemView);
+    }
+}
