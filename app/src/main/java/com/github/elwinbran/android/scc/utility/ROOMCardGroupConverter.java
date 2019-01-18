ea272fe4155db5d31940296415c0671e3967a895
ElwinBran <elwinbran@gmail.com>
2019-01-18 10:54:00 +0100

    Made a 'deserializer' for CardGroups

---
+++ app/src/main/java/com/github/elwinbran/android/scc/utility/ROOMCardGroupConverter.java
@@ -0,0 +1,51 @@
+package com.github.elwinbran.android.scc.utility;
+
+import android.arch.core.util.Function;
+
+import com.github.elwinbran.android.scc.Card;
+import com.github.elwinbran.android.scc.CardGroups;
+import com.github.elwinbran.android.scc.backend.ROOMCard;
+import com.github.elwinbran.android.scc.backend.ROOMCardGroups;
+import com.github.elwinbran.android.scc.utility.dimplementations.MappedCardGroups;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Converter from pojo CardGroups to domain card groups.
+ *
+ * @author Elwin Slokker
+ */
+public class ROOMCardGroupConverter implements Function<ROOMCardGroups, CardGroups>
+{
+    private final Function<ROOMCard, Card> converter = new ROOMCardConverter();
+
+    @Override
+    public CardGroups apply(ROOMCardGroups input)
+    {
+        Map<String, List<ROOMCard>> pojoGroupsMap = input.getCardMap();
+        Map<String, List<Card>> mapGroups = new HashMap<>();
+        for(String key : pojoGroupsMap.keySet())
+        {
+            List<ROOMCard> pojoGroup = pojoGroupsMap.get(key);
+            List<Card> converted;
+            if(pojoGroup == null)
+            {
+                converted = Collections.EMPTY_LIST;
+            }
+            else
+            {
+                converted = new ArrayList<>();
+                for(ROOMCard pojoCard : pojoGroup)
+                {
+                    converted.add(converter.apply(pojoCard));
+                }
+            }
+            mapGroups.put(key, converted);
+        }
+        return new MappedCardGroups(mapGroups);
+    }
+}
