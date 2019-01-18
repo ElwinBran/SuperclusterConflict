71f4dab5217e7c4df5a9b4e3be1f5dc1c6794e50
ElwinBran <elwinbran@gmail.com>
2019-01-18 09:31:30 +0100

    Made a simple implementation for numeric board values.

---
+++ app/src/main/java/com/github/elwinbran/android/scc/utility/dimplementations/DefaultMapGameNumbers.java
@@ -0,0 +1,36 @@
+package com.github.elwinbran.android.scc.utility.dimplementations;
+
+import com.github.elwinbran.android.scc.GameNumbers;
+import com.github.elwinbran.android.scc.PossibleInteger;
+
+import java.util.Map;
+
+/**
+ * The numeric values of a boardgame that are global.
+ *
+ * @author Elwin Slokker
+ */
+public class DefaultMapGameNumbers implements GameNumbers
+{
+    private final Map<String, Integer> wrappedMap;
+
+    /**
+     *  Primary constructor.
+     *
+     * @param map The underlying map used for the number values.
+     *            Consider passing a copy to prevent unexpected behaviour.
+     *            (this implementation does not copy maps.)
+     */
+    public DefaultMapGameNumbers(Map<String, Integer> map)
+    {
+        wrappedMap = map;
+    }
+
+    @Override
+    public PossibleInteger counterValue(String s)
+    {
+        Integer result = wrappedMap.get(s);
+        Boolean present = (result == null)? false : true;
+        return new DefaultPossibleInteger(result, present);
+    }
+}
