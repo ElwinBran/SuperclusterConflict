daacce5d08f00aa9ea66d84e1b6f8487921cc767
ElwinBran <elwinbran@gmail.com>
2019-01-22 10:09:35 +0100

    Added the win activity that will become important later on after fixing the drag and drop.

---
+++ app/src/main/java/com/github/elwinbran/android/scc/app/DemoWonActivity.java
@@ -0,0 +1,12 @@
+package com.github.elwinbran.android.scc.app;
+
+/**
+ * TODO: WIP
+ * Lets the user know the demo is won and completed.
+ *
+ * @author Elwin Slokker
+ */
+public class DemoWonActivity extends FullscreenCompatActivity
+{
+
+}
