51b4a69d4c54c575fc2a25dadf4cc991e57f978f
ElwinBran <elwinbran@gmail.com>
2019-01-24 11:01:57 +0100

    Finished win activity

--- app/src/main/java/com/github/elwinbran/android/scc/app/DemoWonActivity.java
+++ app/src/main/java/com/github/elwinbran/android/scc/app/DemoWonActivity.java
@@ -1,12 +1,30 @@
 package com.github.elwinbran.android.scc.app;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 /**
- * TODO: WIP
  * Lets the user know the demo is won and completed.
  *
  * @author Elwin Slokker
  */
 public class DemoWonActivity extends FullscreenCompatActivity
 {
-
+    @Override
+    public void onCreate(Bundle savedInstance)
+    {
+        setContentView(R.layout.activity_demo_won);
+        super.assign(findViewById(R.id.win_root));
+        super.onCreate(savedInstance);
+        Button confirmButton = findViewById(R.id.button_confirm_win);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(DemoWonActivity.this, StartMenu.class));
+            }
+        });
+    }
 }
